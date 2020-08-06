5d964265a637c36fc9c3c6c4827a13845362d888
Osama-Mattar <osama01145@fci.zu.edu.eg>
2020-08-06 16:03:16 +0200

    init

---
+++ app/src/main/java/com/mattar/osama_mattar_android_test/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.mattar.osama_mattar_android_test
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
