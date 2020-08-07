84937d056765593f54384e3bb1be15f4beeb3351
Osama-Mattar <osama01145@fci.zu.edu.eg>
2020-08-07 10:29:02 +0200

    create Recipe.kt data model

---
+++ app/src/main/java/com/mattar/osama_mattar_android_test/model/data/Recipe.kt
@@ -0,0 +1,31 @@
+package com.mattar.osama_mattar_android_test.model.data
+
+data class Recipe(
+    val calories: String,
+    val carbos: String,
+    val card: String,
+    val country: String,
+    val deliverable_ingredients: List<String>,
+    val description: String,
+    val difficulty: Int,
+    val fats: String,
+    val favorites: Int,
+    val fibers: String,
+    val headline: String,
+    val highlighted: Boolean,
+    val id: String,
+    val image: String,
+    val incompatibilities: String,
+    val ingredients: List<String>,
+    val keywordswords: List<String>,
+    val name: String,
+    val products: List<String>,
+    val proteins: String,
+    val rating: Int,
+    val ratings: Int,
+    val thumb: String,
+    val time: String,
+    val undeliverable_ingredients: List<String>,
+    val user: User,
+    val weeks: List<String>
+)
---
+++ app/src/main/java/com/mattar/osama_mattar_android_test/model/data/User.kt
@@ -0,0 +1,7 @@
+package com.mattar.osama_mattar_android_test.model.data
+
+data class User(
+    val email: String,
+    val latlng: String,
+    val name: String
+)
