8ad78fb175078ec61cc25619b4f002a96b7ebdb0
Osama-Mattar <osama01145@fci.zu.edu.eg>
2020-08-07 04:58:33 +0200

    create gradle kotlin-dsl project level, app module

---
+++ app/build.gradle.kts
@@ -0,0 +1,46 @@
+plugins {
+    id(GradlePluginId.ANDROID_APPLICATION)
+    id(GradlePluginId.KOTLIN_ANDROID)
+    id(GradlePluginId.KOTLIN_ANDROID_EXTENSIONS)
+}
+android {
+    compileSdkVersion(AndroidConfig.COMPILE_SDK_VERSION)
+    buildToolsVersion(AndroidConfig.BUILD_TOOLS_VERSION)
+
+    defaultConfig {
+        applicationId = AndroidConfig.ID
+        minSdkVersion(AndroidConfig.MIN_SDK_VERSION)
+        targetSdkVersion(AndroidConfig.TARGET_SDK_VERSION)
+        versionCode = AndroidConfig.VERSION_CODE
+        versionName = AndroidConfig.VERSION_NAME
+
+        testInstrumentationRunner = AndroidConfig.TEST_INSTRUMENTATION_RUNNER
+    }
+
+    buildTypes {
+        getByName(BuildType.RELEASE) {
+            isMinifyEnabled = BuildTypeRelease.isMinifyEnabled
+            proguardFiles("proguard-android-optimize.txt", "proguard-rules.pro")
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+
+    kotlinOptions {
+        jvmTarget = JavaVersion.VERSION_1_8.toString()
+    }
+}
+
+dependencies {
+    implementation(LibraryDependency.KOTLIN)
+    implementation(LibraryDependency.CORE_KTX)
+    implementation(LibraryDependency.APP_COMPAT)
+    implementation(LibraryDependency.MATERIAL)
+    implementation(LibraryDependency.SUPPORT_CONSTRAINT_LAYOUT)
+    testImplementation(TestLibraryDependency.JUNIT)
+    androidTestImplementation(TestLibraryDependency.ANDROID_X_JUNIT)
+    androidTestImplementation(TestLibraryDependency.TEST_RUNNER)
+    androidTestImplementation(TestLibraryDependency.ESPRESSO_CORE)
+}
