52857f71e28a76a89029431d23337c2b8d852a65
Osama-Mattar <osama01145@fci.zu.edu.eg>
2020-08-10 05:40:44 +0200

    create InjectorUtils.kt for dependency creation

---
+++ app/src/main/java/com/mattar/osama_mattar_android_test/utilities/InjectorUtils.kt
@@ -0,0 +1,23 @@
+package com.mattar.osama_mattar_android_test.utilities
+
+import androidx.fragment.app.Fragment
+import com.mattar.osama_mattar_android_test.model.data.Recipe
+import com.mattar.osama_mattar_android_test.model.repository.RecipeRepositoryImpl
+import com.mattar.osama_mattar_android_test.recipedetails.viewmodel.RecipeDetailsViewModelFactory
+import com.mattar.osama_mattar_android_test.recipeslist.viewmodel.RecipesListViewModelFactory
+
+object InjectorUtils {
+    fun provideRecipesListViewModelFactory(fragment: Fragment): RecipesListViewModelFactory {
+        return RecipesListViewModelFactory(getRecipeRepository(), fragment)
+    }
+
+    private fun getRecipeRepository(): RecipeRepositoryImpl {
+        return RecipeRepositoryImpl.getInstance()
+    }
+
+    fun provideRecipeDetailViewModelFactory(
+        recipe: Recipe
+    ): RecipeDetailsViewModelFactory {
+        return RecipeDetailsViewModelFactory(recipe)
+    }
+}
